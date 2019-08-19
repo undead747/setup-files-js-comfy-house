87cd5971d03cf3b9c5e732eb19d770a9bde739a4
undead747 <narutominh747@gmail.com>
2019-08-19 09:47:11 +0700

    SynData

--- app.js
+++ app.js
@@ -13,7 +13,24 @@ let cart = [];
 
 //getting the product 
 class Products{
- 
+ async getProducts(){
+     try { 
+    let result =await fetch('products.json');
+    let data = await result.json();
+    let products = data.items;
+    products = products.map(item => {
+        const {title,price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+
+        return {title,price,id,image};
+    })
+
+    return products;
+     }catch (error){
+         console.log(error);
+     }
+}
 }
 
 //display products
@@ -30,4 +47,7 @@ class Storage{
 document.addEventListener("DOMContentLoaded",()=>{
     const ui = new UI();
     const products = new Products();
+
+    // get all product
+    products.getProducts().then(data => console.log(data));
 })
